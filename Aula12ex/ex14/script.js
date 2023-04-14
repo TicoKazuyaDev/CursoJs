53f882c513ad0f5189779e103ff738e7d3fa47ae
Tiago Costa <89107071+TicoKazuyaDev@users.noreply.github.com>
2023-04-14 09:19:59 -0300

    vai dfert5r

---
+++ Aula12ex/ex14/script.js
@@ -0,0 +1,20 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+if(hora >=0 && hora <=12){
+    //bom dia
+    img.src = 'manha.png'
+    window.document.body.style.background = '#F0E68C'
+}else if(hora >=12 && hora <= 18){
+    //Boa tarde
+    img.src = 'tarde.png'
+    window.document.body.style.background ='#e2725b'
+}else{
+    //boa noite
+    img.src = 'noite.png'
+    window.document.body.style.background ='#4682B4'
+}
+}
